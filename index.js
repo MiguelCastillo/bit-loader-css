7fee8a060b5771f5f69b299e61b0b736730bf8ea
Miguel Castillo <manchagnu@gmail.com>
2016-01-08 14:35:25 -0500

    refactor to use bit-plugin-builder

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var Plugin = require("bit-bundler-utils/plugin");
-var utils = require("belty");
+var PluginBuilder = require("bit-plugin-builder");
 
 // For now we will use the full path as the name while this
 // issue is resolved:
@@ -9,22 +8,20 @@ var loadStyleName = __dirname + "/loadstyle.js";
 var defaults = {
   match: {
     path: /[\w]+\.(css)$/
+  },
+  dependency: function cssDependency(meta) {
+    return {
+      deps: [loadStyleName],
+      source: "require(\"" + loadStyleName + "\")(" + JSON.stringify(meta.source) + ");"
+    };
   }
 };
 
-function dependencyCSS(meta) {
-  return {
-    deps: [loadStyleName],
-    source: "require(\"" + loadStyleName + "\")(" + JSON.stringify(meta.source) + ");"
-  };
-}
-
 function cssPlugin(options) {
-  var plugin = Plugin.create(utils.merge({}, defaults, options));
-
-  return Plugin.configure(plugin, {
-    dependency: dependencyCSS
-  });
+  return PluginBuilder
+    .create(defaults)
+    .configure(options)
+    .build();
 }
 
 module.exports = cssPlugin;
